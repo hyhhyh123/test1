07a1b5ef9ab2830c3bdb6e1eea2bb0f9a5dc68bc
hyh <2714690577@qq.com>
2023-04-12 19:56:56 +0800

    Initial commit

---
+++ src/com/maoxiaoyu/hyh/test1.java
@@ -0,0 +1,11 @@
+package com.maoxiaoyu.hyh;
+
+/**
+ * @version 17.0
+ * @auther 猫小鱼
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
