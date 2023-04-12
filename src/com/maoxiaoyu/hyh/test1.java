ea220d0a6f817edd5a178832333fa846e5b97ebf
猫小鱼 <122292704+hyhhyh123@users.noreply.github.com>
2023-04-12 20:02:35 +0800

    pull commit

--- src/com/maoxiaoyu/hyh/test1.java
+++ src/com/maoxiaoyu/hyh/test1.java
@@ -8,5 +8,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("1234");
+        System.out.println("12345");
     }
 }
